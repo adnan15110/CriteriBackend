00dcb708931b2c01f726a2474db2745172f69e02
adnankhan <adnankhan@wawanesa.com>
2018-07-18 14:57:02 -0500

    basic backend setup for user model.

--- CriteriBackend/settings.py
+++ CriteriBackend/settings.py
@@ -121,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Rest Api related Settings
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
