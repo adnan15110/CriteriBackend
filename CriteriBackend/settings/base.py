6a9eb338924194574a68c1407e8fe8e9b5001345
adnan <khan.adnanalam@gmail.com>
2018-07-26 14:13:26 -0500

    Updated Static file settings

--- CriteriBackend/settings/base.py
+++ CriteriBackend/settings/base.py
@@ -99,12 +99,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join('staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
-
 
 # Rest Api related Settings
 
