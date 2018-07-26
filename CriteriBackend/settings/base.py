73c20f475e1c76fa36aae91798953b0f00b68e6f
adnan <khan.adnanalam@gmail.com>
2018-07-26 13:41:25 -0500

    Setting up project for Heroku

--- CriteriBackend/settings/base.py
+++ CriteriBackend/settings/base.py
@@ -14,12 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print(BASE_DIR)
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6nehypd6v$a923!9)+6eqqpdl22swn-0_b=t6*kyf_q8e6o&l9'
 
 SITE_ID = 1
 
@@ -105,7 +99,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Rest Api related Settings
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CriteriBackend.wsgi --log-file -
