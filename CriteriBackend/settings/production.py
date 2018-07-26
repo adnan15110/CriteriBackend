cdbc39a0bac732d15b7020f173e1a3dd479325b6
adnan <khan.adnanalam@gmail.com>
2018-07-26 14:08:35 -0500

    Updated the DJANGO_SETTINGS_MODULE

--- CriteriBackend/settings/production.py
+++ CriteriBackend/settings/production.py
@@ -4,22 +4,19 @@ from decouple import config
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['criteri-backend.herokuapp.com',]
+ALLOWED_HOSTS = ['criteri-backend.herokuapp.com', ]
 
 INSTALLED_APPS += []
 
-MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddlewares', ]
 
-print("---------------------")
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = '6nehypd6v$a923!9)+6eqqpdl22swn-0_b=t6*kyf_q8e6o&l9'
+SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
--- CriteriBackend/wsgi.py
+++ CriteriBackend/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CriteriBackend.CriteriBackend.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CriteriBackend.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
