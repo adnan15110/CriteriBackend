a4bfef04df3eb31c3dd49af6e83de8ee56f32806
adnan <khan.adnanalam@gmail.com>
2018-07-27 12:37:54 -0500

    Setting up the django settings related part.
    (cherry picked from commit c9b9e21)

--- CriteriBackend/settings/development.py
+++ CriteriBackend/settings/development.py
@@ -1,14 +1,13 @@
 from CriteriBackend.settings.base import *
 
-TEST_DATA_DIR = os.path.join(BASE_DIR, 'TestData')
-DEBUG = True
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6nehypd6v$a923!9)+6eqqpdl22swn-0_b=t6*kyf_q8e6o&l9'
-ALLOWED_HOSTS = []
-INSTALLED_APPS += []
+MODULE='Development'
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+DEBUG = os.environ.get('DEBUG', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS += []
 
 DATABASES = {
     'default': {
@@ -17,18 +16,20 @@ DATABASES = {
     }
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-AWS_ACCESS_KEY_ID = 'AKIAJZV5K4JX77QARCUQ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'criteri-static'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static'
-
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'CriteriBackend.storage_backends.MediaStorage'
--- CriteriBackend/settings/production.py
+++ CriteriBackend/settings/production.py
@@ -2,6 +2,7 @@ from CriteriBackend.settings.base import *
 import dj_database_url
 from decouple import config
 
+MODULE='Production'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['criteri-backend.herokuapp.com', ]
@@ -11,6 +12,7 @@ INSTALLED_APPS += []
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware', ]
 
 SECRET_KEY = config('SECRET_KEY')
+
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
@@ -18,6 +20,24 @@ DATABASES = {
     )
 }
 
+# EMAIL BACKEND
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# STATIC STORAGES
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_URL = '/static/'
+
+# MEDIA STORAGES
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'CriteriBackend.storage_backends.MediaStorage'
