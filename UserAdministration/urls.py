fb7a0c30732e898f5da456eea9cc7c3d010981a1
adnan <khan.adnanalam@gmail.com>
2018-07-27 17:04:52 -0500

    updated the base name in the url and attached profile queryset.

--- UserAdministration/urls.py
+++ UserAdministration/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from UserAdministration.views import ProfileViewSet, UserViewSet, AddressViewSet
 
 user_admin_router=DefaultRouter()
-user_admin_router.register('profile', ProfileViewSet)
-user_admin_router.register('user', UserViewSet)
-user_admin_router.register('address', AddressViewSet)
+user_admin_router.register('profile', ProfileViewSet, base_name='profile')
+user_admin_router.register('user', UserViewSet, base_name='user')
+user_admin_router.register('address', AddressViewSet, base_name='address')
